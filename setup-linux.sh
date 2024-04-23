98c0284f214755116d579bf31808c22202cc135c
yurajp <metanol00@list.ru>
2024-04-23 14:40:23 +0300

    setup scripts added

---
+++ setup-linux.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+[ ! -d data ] && mkdir -p data
+[ ! -d server/files/avatars ] && mkdir -p server/files/avatars
+[ ! -f data/auth ] && touch data/auth
+go build
+
+sh='#!'
+cdp="cd "
+andp=" && ./wx && sleep(4)"
+touch wx.sh
+echo $sh$SHELL>wx.sh
+echo $cdp$(pwd)$andp>>wx.sh
+chmod ug+x wx.sh
+
+echo "Please edit file 'config/conf.ini' to define path to your certificates"
--- termux-build.sh
+++
@@ -1,9 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash
-
-[ ! -d data ] && mkdir -p data
-[ ! -d server/files/avatars ] && mkdir -p server/files/avatars
-[ ! -f data/auth ] && touch data/auth
-go build
-
-echo "You should edit the configuration file 'config/conf.ini' to define path to your certificates"
-
